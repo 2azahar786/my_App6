18ce4709b506ac29e88310f3f4cbbce52b4d15de
2azahar786 <2azahar786@gmail.com>
2023-12-01 01:04:00 +0530

    style added to YouTube file

--- src/Components/YouTube.js
+++ src/Components/YouTube.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const YouTube = () => {
     return(
-        <div>
+        <div className =' h-100px w-100px bg-gray-400 text-white'>
             <h1>Do you want to watch movies</h1>
             <a href="https://www.youtube.com/@varsitybyzerodha/playlists">Click here to Search any Movies</a>
         </div>
